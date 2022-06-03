be34b48d5be4fa0479c8653d14d404e6987ca07d
Yeow Heng <20042741@myrp.edu.sg>
2022-06-03 11:48:51 +0800

    completed spinner

--- app/src/main/java/sg/edu/rp/c346/id20042741/demospinnertest/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20042741/demospinnertest/MainActivity.java
@@ -3,12 +3,43 @@ package sg.edu.rp.c346.id20042741.demospinnertest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView output;
+    Spinner spnYesNo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        output = findViewById(R.id.selectedItem);
+        spnYesNo = findViewById(R.id.spinnerId);
+
+        spnYesNo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch(position){
+                    case 0:
+                        output.setText("Spinnner item: Yes selected");
+                        break;
+                    case 1:
+                        output.setText("Spinner item: No selected");
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
+
+
 }
